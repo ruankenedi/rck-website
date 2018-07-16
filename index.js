843192d02abfe2335be060a4ddc3d14e3058c135
ruankenedi <ruankenedi01@gmail.com>
2018-07-16 20:27:44 -0300

    First commit

---
+++ index.js
@@ -0,0 +1,18 @@
+// STUFFS.JS
+import './js/bootstrap.min';
+import './js/custom';
+import './js/jquery';
+import './js/jquery.magnific-popup.min';
+import './js/magnific-popup-options';
+import './js/owl.carousel.min';
+import './js/smoothscroll';
+import './js/wow.min';
+
+// STUFFS.CSS
+import './css/animate.css';
+import './css/bootstrap.min.css';
+import './css/font-awesome.min.css';
+import './css/magnific-popup.css';
+import './css/owl.carousel.css';
+import './css/owl.theme.css';
+import './css/style.css';
---
+++ webpack.config.js
@@ -0,0 +1,83 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  entry: {
+    index: './index'
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist/'),
+    publicPath: '/',
+    filename: 'js/[name].[hash].bundle.js'
+  },
+  plugins: [
+    new CleanWebpackPlugin('./dist'),
+    new HtmlWebpackPlugin({
+      template: './index.html',
+      chunks: ['index'],
+      favicon: './images/home-bg.jpg',
+      hash: true,
+      filename: path.resolve(__dirname, 'dist/index.html')
+    }),
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+      'window.jQuery': 'jquery',
+      'window.$': 'jquery'
+    }),
+    new ExtractTextPlugin('styles.css'),
+    new UglifyJSPlugin()
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+            options: {
+              minimize: true,
+              removeComments: true,
+              collapseWhitespace: true
+            }
+          }
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            {
+              loader: 'css-loader',
+              options: {
+                minimize: true
+              }
+            }
+          ]
+        })
+      },
+      {
+        test: /\.(png|jpg|jpeg|mp4|mpeg|gif|ico|svg|ttf|eot|woff|woff2)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[hash].[ext]',
+              outputPath: 'assets/'
+            }
+          }
+        ]
+      }
+    ]
+  }
+};
